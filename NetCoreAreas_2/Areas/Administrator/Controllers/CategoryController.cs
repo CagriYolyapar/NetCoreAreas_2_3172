723483807a4c5be8fb0527648df386279010d5a0
cagri.yolyapar <cagri.yolyapar@gmail.com>
2024-09-05 10:31:56 +0300

    Add project files.

---
+++ NetCoreAreas_2/Areas/Administrator/Controllers/CategoryController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace NetCoreAreas_2.Areas.Administrator.Controllers
+{
+    //Area'lar Attribute olarak tanımlanmadan calıstırılamaz...
+
+    [Area("Administrator")]
+
+    public class CategoryController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
